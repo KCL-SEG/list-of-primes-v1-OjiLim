ae64e6721a2395c03131d741561e8f17c40f62d5
Ryan Lim Yi Ming (github acc) <k21001230@kcl.ac.uk>
2022-10-04 03:22:40 +0100

    Add exception throwing to both funtions
    Throws ValueError
    Sieve_Of_Eratosthenes_Number_Of_Primes accepts any non negative integer including zero
    Sieve_Of_Eratosthenes accepts any non negative integer excluding zero

--- primes.py
+++ primes.py
@@ -4,14 +4,16 @@ def primes(number_of_primes):
     list = Sieve_Of_Eratosthenes_Number_Of_Primes(number_of_primes)
     return list
 
-#complicated way for learning syntax!!
+#complicated way for learning syntax!! finds all prime numbers uptill number entered
 def Sieve_Of_Eratosthenes(largestNumber):
+    if largestNumber<=0:
+        raise ValueError("Non negative numbers allowed, excluding zero")
     numbers = [True] * (largestNumber+1) #making list with all true values
     numbers[0] = False
     numbers[1] = False
     for i in range(2,math.ceil(largestNumber**0.5),1):
         if numbers[i] == True:
-            for j in range(i**2,largestNumber,i): #removes numbers via i**2+ni where n is an integer
+            for j in range(i**2,largestNumber+1,i): #removes numbers via i**2+ni where n is an integer
                 numbers[j] = False
     primeNumbers = []
     for index, value in enumerate(numbers): #gets index of true values which equate to prime
@@ -21,6 +23,8 @@ def Sieve_Of_Eratosthenes(largestNumber):
 
 #Same as above but uses number of primes in list instead(Brute force method used, prime number theorem could be used as basis instead reducing complexity)
 def Sieve_Of_Eratosthenes_Number_Of_Primes(numberOfPrimes):
+    if numberOfPrimes<0:
+        raise ValueError("Non negative numbers allowed, including zero")
     primeNumbers = []
     largestNumber = 2
     while len(primeNumbers) != numberOfPrimes:
