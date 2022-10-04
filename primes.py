8ff6352e69740191aa6f959661c11ddf7823edef
Ryan Lim Yi Ming (github acc) <k21001230@kcl.ac.uk>
2022-10-04 03:09:21 +0100

    Added Sieve_Of_Eratosthenes funtions
    Done on the basis of learning
    Technically the brute force way of finding primes via modulo 0 could be use, but i tried to spice things up!!!

--- primes.py
+++ primes.py
@@ -1,6 +1,39 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
+import math
 def primes(number_of_primes):
-    list = []
+    list = Sieve_Of_Eratosthenes_Number_Of_Primes(number_of_primes)
     return list
+
+#complicated way for learning syntax!!
+def Sieve_Of_Eratosthenes(largestNumber):
+    numbers = [True] * (largestNumber+1) #making list with all true values
+    numbers[0] = False
+    numbers[1] = False
+    for i in range(2,math.ceil(largestNumber**0.5),1):
+        if numbers[i] == True:
+            for j in range(i**2,largestNumber,i): #removes numbers via i**2+ni where n is an integer
+                numbers[j] = False
+    primeNumbers = []
+    for index, value in enumerate(numbers): #gets index of true values which equate to prime
+        if value == True:
+            primeNumbers.append(index)
+    return primeNumbers
+
+#Same as above but uses number of primes in list instead(Brute force method used, prime number theorem could be used as basis instead reducing complexity)
+def Sieve_Of_Eratosthenes_Number_Of_Primes(numberOfPrimes):
+    primeNumbers = []
+    largestNumber = 2
+    while len(primeNumbers) != numberOfPrimes:
+        primeNumbers = []
+        numbers = [True] * (largestNumber+1)
+        numbers[0] = False
+        numbers[1] = False
+        for i in range(2,math.ceil(largestNumber**0.5),1):
+            if numbers[i] == True:
+                for j in range(i**2,largestNumber+1,i):
+                    numbers[j] = False
+        for index, value in enumerate(numbers):
+            if value == True:
+                primeNumbers.append(index)
+        largestNumber+=1
+    return primeNumbers
